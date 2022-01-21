26e5e82bd79d9d3df6f786b59c874452f802e492
zzy <zhouhoushu@126.com>
2022-01-21 10:08:42 +0000

    replace <a> with <Link>

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,28 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './style.css'
 
 const NavBar = () => {
   return (
-    <div className='wrap'>
+    <div className="wrap">
       <ul className="topnav">
         <li>
-          <a className="site-title" href="home">
+          <Link to="/home" className="site-title">
             球队颜色
-          </a>
+          </Link>
         </li>
         <div className="right">
           <li>
-            <a href="cba">CBA</a>
+            <Link to="/cba">CBA</Link>
           </li>
           <li>
-            <a href="csa">中超</a>
+            <Link to="/csa">中超</Link>
           </li>
           <li>
-            <a href="esports">电竞</a>
+            <Link to="/esports">电竞</Link>
           </li>
           <li>
-            <a href="more">更多</a>
+            <Link to="/more">更多</Link>
           </li>
         </div>
       </ul>
