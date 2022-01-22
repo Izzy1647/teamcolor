937f21bfce9899cf415c398a1af313c318bb50e2
zzy <zhouhoushu@126.com>
2022-01-22 23:43:41 +0000

    pick font color based on background lightness

--- src/modules/League/Teamlist/index.js
+++ src/modules/League/Teamlist/index.js
@@ -3,6 +3,19 @@ import { Link } from 'react-router-dom'
 
 import './style.css'
 
+const isLightColor = color => {
+  const hex = color.replace('#', '')
+  const r = parseInt(hex.substr(0, 2), 16)
+  const g = parseInt(hex.substr(2, 2), 16)
+  const b = parseInt(hex.substr(4, 2), 16)
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000
+  return brightness > 155
+}
+
+const titleStyle = isLightColor => {
+  return isLightColor ? { color: 'black' } : { color: 'white' }
+}
+
 const Teamlist = ({ teams }) => {
   return (
     <>
@@ -14,7 +27,12 @@ const Teamlist = ({ teams }) => {
             style={{ background: team.theme }}
           >
             <Link to={team.link}>
-              <p className="team-title">{team.name}</p>
+              <p
+                className="team-title"
+                style={titleStyle(isLightColor(team.theme))}
+              >
+                {team.name}
+              </p>
             </Link>
           </div>
         )
