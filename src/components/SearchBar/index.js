3641bcb1c325b98e1a180e1d24081e890f2099cc
zzy <zhouhoushu@126.com>
2022-01-27 19:36:30 +0000

    implement search for team name in home page

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import search from '../../utils/search'
 
 import './style.css'
 
 const SearchBar = () => {
+  const navigate = useNavigate()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const team = document.getElementById('team').value
+    const redirect = search(team)
+    navigate(redirect)
+  }
+
   return (
-    <section className='searchbar-container'>
-      <div className='search-widget-wrap'>
-        <p className='search-widget-title'>查找你的球队</p>
-        <input className='search-form-input' placeholder='网站中查找'/>
+    <section className="searchbar-container">
+      <div className="search-widget-wrap">
+        <p className="search-widget-title">查找你的球队</p>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="search-form-input"
+            placeholder="网站中查找"
+            id="team"
+            type="text"
+          />
+        </form>
       </div>
     </section>
   )
---
+++ src/utils/search.js
@@ -0,0 +1,15 @@
+import { cbaTeams } from '../data/cba/teams'
+
+/**
+ * search the input string in team data, return the target url for redirect
+ *
+ * @param {string} team
+ * @returns {string}
+ */
+export default function search(team) {
+  const target = cbaTeams.find(item => {
+    return item.full.includes(team) || item.name.includes(team)
+  })
+
+  return target ? target.link : '/no-match'
+}
