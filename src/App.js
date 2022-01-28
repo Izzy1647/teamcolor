5c62cafcafa0ab2d7bb4a43fa366a314b6547332
zzy <zhouhoushu@126.com>
2022-01-28 21:45:19 +0000

    wrap app in PUBLIC_URL

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Oops from './modules/Oops'
 
 export default function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <NavBar />
       <div style={{ padding: '3.8rem' }}>
         <Routes>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 import 'antd/dist/antd.css'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
