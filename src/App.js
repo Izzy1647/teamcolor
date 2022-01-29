4f7c2e6852fb2fcbc3857f322642b9829fdb3a1e
zzy <zhouhoushu@126.com>
2022-01-29 15:05:13 +0000

    mobile support for home page

--- src/App.js
+++ src/App.js
@@ -7,18 +7,19 @@ import League from './modules/League'
 import Oops from './modules/Oops'
 
 export default function App() {
+  const isMobileScreen = window.matchMedia('(max-width:500px)').matches
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <NavBar />
-      <div style={{ padding: '3.8rem' }}>
+      <div style={!isMobileScreen ? { padding: '3.8rem' } : { padding: 0 }}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cba" element={<League type="cba" />} />
           <Route path="/cba/:team" element={<Detail />} />
-          <Route path="/csl" element={<Oops type="noSupport"/>} />
+          <Route path="/csl" element={<Oops type="noSupport" />} />
           <Route path="/esports" element={<Oops type="noSupport" />} />
-          <Route path="/more" element={<Oops type="noSupport"/>} />
-          <Route path="/no-match" element={<Oops type="noMatch"/>} />
+          <Route path="/more" element={<Oops type="noSupport" />} />
+          <Route path="/no-match" element={<Oops type="noMatch" />} />
         </Routes>
       </div>
     </Router>
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import { Menu, Dropdown } from 'antd'
 import { Link } from 'react-router-dom'
+import { BiMenu } from 'react-icons/bi'
 
 import './style.css'
 
+const menu = (
+  <Menu>
+    <Menu.Item key="0">
+      <Link to="/cba">CBA</Link>
+    </Menu.Item>
+    <Menu.Item key="1">
+      <Link to="/csl">中超</Link>
+    </Menu.Item>
+    <Menu.Item key="2">
+      <Link to="/esports">电竞</Link>
+    </Menu.Item>
+  </Menu>
+)
+
 const NavBar = () => {
+  const isMobileScreen = window.matchMedia('(max-width:500px)').matches
+
   return (
     <div className="wrap">
-      <ul className="topnav" style={{margin: 6}}>
+      <ul className="topnav" style={{ margin: 6 }}>
         <li>
           <Link to="/" className="site-title">
             球队颜色
@@ -26,6 +44,19 @@ const NavBar = () => {
             <Link to="/more">更多</Link>
           </li>
         </div>
+        {isMobileScreen && (
+          <li style={{"marginLeft": "auto"}}>
+            <Dropdown
+              overlay={menu}
+              trigger={['click']}
+              placement="bottomRight"
+            >
+              <span>
+                <BiMenu />
+              </span>
+            </Dropdown>
+          </li>
+        )}
       </ul>
     </div>
   )
