4b6971276085e192995f10df7135487c534ff00c
zzy <zhouhoushu@126.com>
2022-02-05 20:29:25 +0000

    cba team color data updates

--- src/data/cba/teams.js
+++ src/data/cba/teams.js
@@ -21,15 +21,15 @@ export const cbaTeams = [
     full: '北京紫禁勇士俱乐部',
     theme: '#4A2478',
     link: '/cba/beikong',
-    colors: ['#4A2478', '#FFD700', '#000000']
+    colors: ['#4A2478', '#FFD700', '#FFFFFF', '#C5A15E']
   },
   {
     key: 'beijing',
     name: '北京首钢',
     full: '北京鸭首钢俱乐部',
-    theme: '#6C99C3',
+    theme: '#3375C4',
     link: '/cba/beijing',
-    colors: ['#6C99C3', '#000000', '#FFFFFF']
+    colors: ['#3375C4', '#FFFFFF', '#564E97', '#E8B65D']
   },
   {
     key: 'jilin',
@@ -37,7 +37,7 @@ export const cbaTeams = [
     full: '吉林九台农商行东北虎俱乐部',
     theme: '#FF0000',
     link: '/cba/jilin',
-    colors: ['#FF0000', '#FFD700', '#FFFFFF']
+    colors: ['#FF0000', '#FFD700', '#FFFFFF', '#070404']
   },
   {
     key: 'fujian',
@@ -45,47 +45,47 @@ export const cbaTeams = [
     full: '福建鲟浔兴俱乐部',
     theme: '#1589FF',
     link: '/cba/fujian',
-    colors: ['#1589FF', '#000000']
+    colors: ['#1589FF', '#EA7035', '#FFFFFF', '#000000']
   },
   {
     key: 'guangdong',
     name: '广东东莞大益',
     full: '广东宏远华南虎俱乐部',
-    theme: '#000080',
+    theme: '#19315B',
     link: '/cba/guangdong',
-    colors: ['#000080', '#D40000', '#FFFFFF']
+    colors: ['#19315B', '#D40000', '#FCEF50', '#FFFFFF', '#322C2B']
   },
   {
     key: 'liaoning',
     name: '辽宁本钢',
     full: '辽宁沈阳三生飞豹俱乐部',
-    theme: '#000000',
+    theme: '#3E823E',
     link: '/cba/liaoning',
-    colors: ['#000000', 'FFFFFF', '#FFCC00']
+    colors: ['#3E823E', '#000000', 'FFFFFF', '#E8A439']
   },
   {
     key: 'nanjing',
     name: '南京同曦宙光',
     full: '南京同曦大圣俱乐部',
-    theme: '#FFD700',
+    theme: '#192749',
     link: '/cba/nanjing',
-    colors: ['#FFD700', '#FFFFFF', '#FF0000']
+    colors: ['#192749', '#FFD700', '#FFFFFF', '#FF0000']
   },
   {
     key: 'ningbo',
     name: '宁波甬兴证券',
     full: '宁波富邦火箭俱乐部',
-    theme: '#FFFFFF',
+    theme: '#193F92',
     link: '/cba/ningbo',
-    colors: ['#FFFFFF', '0000FF']
+    colors: ['#193F92', '#FFFFFF']
   },
   {
     key: 'qingdao',
     name: '青岛每日优鲜',
     full: '青岛国信海天雄鹰俱乐部',
-    theme: '#0E2960',
+    theme: '#51B2E9',
     link: '/cba/qingdao',
-    colors: ['#0E2960']
+    colors: ['#51B2E9', '#0E2960', '#BC2621']
   },
   {
     key: 'shanxi',
@@ -99,17 +99,17 @@ export const cbaTeams = [
     key: 'shandong',
     name: '山东高速',
     full: '山东高速麒麟俱乐部',
-    theme: '#FFFFFF', // ...
+    theme: '#7F1D85',
     link: '/cba/shandong',
-    colors: ['#000000', '#FFFFFF']
+    colors: ['#7F1D85', '#FFFFFF', '#1B2E53', '#4395D3', '#D85F28']
   },
   {
     key: 'sichuan',
     name: '四川金荣实业',
     full: '四川金强蓝鲸俱乐部',
-    theme: '#005A87', // ...
+    theme: '#3256C6',
     link: '/cba/sichuan',
-    colors: ['#005A87', '#F6A501', '#000000']
+    colors: ['#3256C6', '#005A87', '#F6A501']
   },
 
   {
@@ -118,7 +118,7 @@ export const cbaTeams = [
     full: '深圳新世纪烈豹俱乐部',
     theme: '#AA2221',
     link: '/cba/shenzhen',
-    colors: ['#AA2221', '#000080']
+    colors: ['#AA2221', '#E7C040']
   },
   {
     key: 'guangzhou',
@@ -126,15 +126,15 @@ export const cbaTeams = [
     full: '广州龙狮俱乐部',
     theme: '#151F42',
     link: '/cba/guangzhou',
-    colors: ['#DF0534', '#151F42', '#000000']
+    colors: ['#151F42', '#DF0534', '#000000']
   },
   {
     key: 'suzhou',
     name: '苏州肯帝亚',
     full: '江苏龙肯帝亚俱乐部',
-    theme: '#2C3CB5',
+    theme: '#F5DA59',
     link: '/cba/suzhou',
-    colors: ['#2C3CB5', '#000000']
+    colors: ['#F5DA59', '#FFFFFF']
   },
   {
     key: 'tianjin',
@@ -142,7 +142,7 @@ export const cbaTeams = [
     full: '天津荣钢先行者俱乐部',
     theme: '#0040AA',
     link: '/cba/tianjin',
-    colors: ['#0040AA', '#DD0830']
+    colors: ['#0040AA', '#DD0830', '#FFFFFF']
   },
   {
     key: 'xinjiang',
@@ -150,7 +150,7 @@ export const cbaTeams = [
     full: '新疆广汇飞虎俱乐部',
     theme: '#87CEEB',
     link: '/cba/xinjiang',
-    colors: ['#87CEEB', '#000000']
+    colors: ['#1C4698', '#87CEEB', '#000000']
   },
   {
     key: 'zhejiang',
@@ -164,8 +164,8 @@ export const cbaTeams = [
     key: 'guangsha',
     name: '浙江广厦控股',
     full: '浙江广厦雄狮俱乐部',
-    theme: '#DDCC00',
+    theme: '#E7BA45',
     link: '/cba/guangsha',
-    colors: ['#DDCC00', '#000000', '#FFFFFF']
+    colors: ['#E7BA45', '#000000', '#FFFFFF']
   }
 ]
