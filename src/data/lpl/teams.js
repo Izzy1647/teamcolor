efc8eafbd7cb0801e65d2f474c1787df5905a3e0
zzy <zhouhoushu@126.com>
2022-02-19 12:29:19 +0000

    add LPL data

---
+++ src/data/lpl/teams.js
@@ -0,0 +1,154 @@
+export const lplTeams = [
+  {
+    key: 'al',
+    name: 'AL',
+    full: 'AnyonesLegend',
+    theme: '#C32E2B',
+    colors: ['#C32E2B', '#D6736E', '#1D120F', '#EBC2B8', '#504A48'],
+    link: '/lpl/al'
+  },
+  {
+    key: 'blg',
+    name: 'BLG平安银行',
+    full: 'BilibiliGaming',
+    theme: '#48BEDF',
+    colors: ['#48BEDF', '#E96E8F'],
+    link: '/lpl/blg'
+  },
+  {
+    key: 'edg',
+    name: 'EDG',
+    full: 'EDWARDGAMING',
+    theme: '#221815',
+    colors: ['#221815', '#E0E0DE', '#716C6A'],
+    link: '/lpl/edg'
+  },
+  {
+    key: 'fpx',
+    name: 'FPX',
+    full: 'FunPlusPhoenix',
+    theme: '#FA0F00',
+    colors: ['#FA0F00'],
+    link: '/lpl/fpx'
+  },
+  {
+    key: 'ig',
+    name: 'IG',
+    full: 'InvictusGaming',
+    theme: '#FFFFFF',
+    colors: ['#FFFFFF', '#040707', '#C1C1C1', '#4B4B4B', '#898989'],
+    link: '/lpl/ig'
+  },
+  {
+    key: 'jdg',
+    name: '北京JDG',
+    full: 'BeijingJingdongGaming',
+    theme: '#C8102E',
+    colors: ['#C8102E'],
+    link: '/lpl/jdg'
+  },
+  {
+    key: 'lgd',
+    name: '杭州LGD',
+    full: 'HangzhouLGDGAMING',
+    theme: '#FF0406',
+    colors: [
+      '#FF0406',
+      '#0D408F',
+      '#FFFFFF',
+      '#FF6465',
+      '#9A1940',
+      '#5176AF',
+      '#97ADCF'
+    ],
+    link: '/lpl/lgd'
+  },
+  {
+    key: 'lng',
+    name: '苏州LNG',
+    full: 'SuzhouLNGEsports',
+    theme: '#DD235B',
+    colors: [
+      '#DD235B',
+      '#19254B',
+      '#307FC2',
+      '#71C8D5',
+      '#EB779E',
+      '#89567B',
+      '#FFFFFF'
+    ],
+    link: '/lpl/lng'
+  },
+  {
+    key: 'omg',
+    name: 'OMG',
+    full: 'OhMyGod',
+    theme: '#000000',
+    colors: ['#000000', '#FFFFFF', '#C2C2C2', '#6D6D6D'],
+    link: '/lpl/omg'
+  },
+  {
+    key: 'ra',
+    name: 'RA',
+    full: '',
+    theme: '#AC4FC6',
+    colors: ['#AC4FC6'],
+    link: '/lpl/ra'
+  },
+  {
+    key: 'rng',
+    name: 'RNG',
+    full: 'RoyalNeverGiveUp',
+    theme: '#B4926A',
+    colors: ['#B4926A'],
+    link: '/lpl/rng'
+  },
+  {
+    key: 'tes',
+    name: 'TES',
+    full: 'TOPESPORTS',
+    theme: '#FF3E24',
+    colors: ['#FF3E24'],
+    link: '/lpl/tes'
+  },
+  {
+    key: 'tt',
+    name: 'TT',
+    full: 'THUNDERTALKGAMING',
+    theme: '#1ED2EC',
+    colors: ['#1ED2EC'],
+    link: '/lpl/tt'
+  },
+  {
+    key: 'up',
+    name: 'UP',
+    full: 'UltraPrime',
+    theme: '#5643DF',
+    colors: ['#5643DF', '#50E6E6', '#9E8FF5', '#FFFFFF', '#3A8A8A', '#212121'],
+    link: '/lpl/up'
+  },
+  {
+    key: 'v5',
+    name: '深圳V5',
+    full: 'ShenzhenVictoryFive',
+    theme: '#B69265',
+    colors: ['#B69265'],
+    link: '/lpl/v5'
+  },
+  {
+    key: 'wbg',
+    name: 'WBG',
+    full: 'WeiboGaming',
+    theme: '#E60012',
+    colors: ['#E60012', '#F39700', '#F4A595', '#FFFFFF', '#221815', '#6B6665'],
+    link: '/lpl/wbg'
+  },
+  {
+    key: 'we',
+    name: '西安WE',
+    full: 'XianTeamWE',
+    theme: '#DF0914',
+    colors: ['#DF0914'],
+    link: '/lpl/we'
+  }
+]
