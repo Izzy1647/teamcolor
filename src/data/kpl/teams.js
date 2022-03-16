06c28b9a5e2bcb9ba5aefb32bf109ca133f68ff3
zzy <zhouhoushu@126.com>
2022-03-16 15:04:17 +0000

    add kpl data

---
+++ src/data/kpl/teams.js
@@ -0,0 +1,191 @@
+export const kplTeams = [
+  {
+    key: 'ag',
+    name: 'AG',
+    full: '',
+    theme: '#E80000',
+    colors: ['#E80000'],
+    link: '/kpl/ag'
+  },
+  {
+    key: 'dyg',
+    name: 'DYG',
+    full: '',
+    theme: '#FF6000',
+    colors: [
+      '#FF6000',
+      '#000F2D',
+      '#912800',
+      '#F99A6B',
+      '#064F8D',
+      '#7EC4FB',
+      '#518FC4',
+      '#684F4D',
+      '#EAEEF3'
+    ],
+    link: '/kpl/dyg'
+  },
+  {
+    key: 'edgm',
+    name: 'EDGM',
+    full: '',
+    theme: '#000000',
+    colors: ['#000000', '#FF0018', '#FFFFFF', '#A19F9F', '#504D4D'],
+    link: '/kpl/edgm'
+  },
+  {
+    key: 'estar',
+    name: 'ESTAR',
+    full: '',
+    theme: '#FFFFFF',
+    colors: ['#FFFFFF'],
+    link: '/kpl/estar'
+  },
+  {
+    key: 'gk',
+    name: 'GK',
+    full: '',
+    theme: '#F43162',
+    colors: ['#F43162'],
+    link: '/kpl/gk'
+  },
+  {
+    key: 'hero',
+    name: 'HERO',
+    full: '',
+    theme: '#552583',
+    colors: ['#552583'],
+    link: '/kpl/hero'
+  },
+  {
+    key: 'ksg',
+    name: 'KSG',
+    full: '',
+    theme: '#EC4C17',
+    colors: [
+      '#EC4C17',
+      '#FFBC2E',
+      '#840100',
+      '#9E813D',
+      '#072129',
+      '#E0FFFF',
+      '#869FA0'
+    ],
+    link: '/kpl/ksg'
+  },
+  {
+    key: 'lang',
+    name: 'LANG',
+    full: '',
+    theme: '#F2B221',
+    colors: [
+      '#F2B221',
+      '#9C6400',
+      '#FFF68C',
+      '#000000',
+      '#D6DFFF',
+      '#7B839F',
+      '#3E4857'
+    ],
+    link: '/kpl/lang'
+  },
+  {
+    key: 'lgd',
+    name: 'LGD',
+    full: '',
+    theme: '#FFFFFF',
+    colors: [
+      '#FFFFFF',
+      '#FD0200',
+      '#00439B',
+      '#EC4A46',
+      '#448DC9',
+      '#8D1A34',
+      '#FF9D97',
+      '#9ABFDE',
+      '#190D4F'
+    ],
+    link: '/kpl/lgd'
+  },
+  {
+    key: 'mtg',
+    name: 'MTG',
+    full: '',
+    theme: '#F5C61C',
+    colors: ['#F5C61C', '#BE1020'],
+    link: '/kpl/mtg'
+  },
+  {
+    key: 'rngm',
+    name: 'RNGM',
+    full: '',
+    theme: '#B7916C',
+    colors: ['#B7916C'],
+    link: '/kpl/rngm'
+  },
+  {
+    key: 'rw',
+    name: 'RW',
+    full: '',
+    theme: '#C8243F',
+    colors: ['#C8243F'],
+    link: '/kpl/rw'
+  },
+  {
+    key: 'tesa',
+    name: 'TESA',
+    full: '',
+    theme: '#F85F04',
+    colors: ['#F85F04'],
+    link: '/kpl/tesa'
+  },
+  {
+    key: 'ttg',
+    name: 'TTG',
+    full: '',
+    theme: '#0B0BAF',
+    colors: ['#0B0BAF', '#6621E3', '#942100', '#B77BF0', '#FFFFFF'],
+    link: '/kpl/ttg'
+  },
+  {
+    key: 'vg',
+    name: 'VG',
+    full: '',
+    theme: '#37312F',
+    colors: ['#37312F', '#A09F9F', '#E0E0DF'],
+    link: '/kpl/vg'
+  },
+  {
+    key: 'wb',
+    name: 'WB',
+    full: '',
+    theme: '#FFCB16',
+    colors: [
+      '#FFCB16',
+      '#F11A23',
+      '#A46905',
+      '#FD8B93',
+      '#65000F',
+      '#9695BB',
+      '#443856',
+      '#D9DCF1'
+    ],
+    link: '/kpl/wb'
+  },
+  {
+    key: 'we',
+    name: 'WE',
+    full: '',
+    theme: '#DD262A',
+    colors: ['#DD262A'],
+    link: '/kpl/we'
+  },
+  {
+    key: 'xyg',
+    name: 'XYG',
+    full: '',
+    theme: '#A09F9F',
+    colors: ['#A09F9F', '#FFFFFF', '#4D4D4D', '#000000'],
+    link: '/kpl/xyg'
+  }
+]
